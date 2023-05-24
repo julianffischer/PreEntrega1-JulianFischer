7cd6a8cc16dc5feecc7078fdbb2a086ecc3345c2
Julian Fischer <fischerjulianroman@gmail.com>
2023-05-24 20:29:50 -0300

    Solucion al problema para subir a git

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import './index.scss';
 import { App } from './App';
 
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App/>
-    <itemListContainer greeting = {0}/>
   </React.StrictMode>
 );
 
